a0d9c73b25487e840c3722588414f20bd86ade89
hadardv <hadardv14@gmail.com>
2025-09-24 10:21:57 +0300

    remove redundant import

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App";
-import "./index.css";
 import { QueryClient } from "@tanstack/react-query";
 
 const queryClient = new QueryClient({
